4727fc0171d2c1d877f4cfa6468da49017a67493
Flamaros <flamaros.xavier@gmail.com>
2019-11-29 22:59:26 +0100

    [Profiling] Add a small speed test.

--- compiler-cpp/sources/main.cpp
+++ compiler-cpp/sources/main.cpp
@@ -6,17 +6,70 @@
 
 #include <utilities/file.hpp>
 #include <utilities/string.hpp>
+#include <utilities/exit_scope.hpp>
 
 #include <iostream>
+#include <chrono>
 
 #include <cstdlib>
+
+#define TEST_PARSING_SPEED
+
+void	test_parsing_speed()
+{
+	typedef std::chrono::high_resolution_clock	Clock;
+	typedef std::chrono::time_point<Clock>		Time_Point;
+
+	Time_Point	start;
+	Time_Point	start_tokenize;
+	Time_Point	start_parse;
+	Time_Point	end;
+	Time_Point	end_tokenize;
+	Time_Point	end_parse;
+
+	start = Clock::now();
+
+	std::string             input_file_content;
+	std::vector<f::Token>   tokens;
+	f::AST					parsing_result;
+	int						result = 0;
+
+	if (utilities::read_all_file("./tests/big_file.f", input_file_content) == true) {
+		start_tokenize = Clock::now();
+		f::tokenize(input_file_content, tokens);
+		end_tokenize = Clock::now();
+
+		start_parse = Clock::now();
+		f::parse(tokens, parsing_result);
+		end_parse = Clock::now();
+	}
+
+	end = Clock::now();
+
+	size_t	nb_lines = tokens.back().line;
+
+	std::chrono::duration<double> tokenize_s = end_tokenize - start_tokenize;
+	std::chrono::duration<double> parse_s = end_parse - start_parse;
+	std::chrono::duration<double> total_s = end - start;
+
+	std::cout << "./tests/big_file.f" << std::endl;
+	std::cout << "    tokenize: " << tokenize_s.count() << " s - lines/s: " << nb_lines / tokenize_s.count() << std::endl;
+	std::cout << "    parse:    " << parse_s.count() << " s - lines/s: " << nb_lines / parse_s.count() << std::endl;
+	std::cout << "    total:    " << total_s.count() << " s - lines/s: " << nb_lines / total_s.count() << std::endl;
+}
 
 int main(int ac, char** av)
 {
     std::string             input_file_content;
     std::vector<f::Token>   tokens;
-    f::AST       parsing_result;
+    f::AST					parsing_result;
 	int						result = 0;
+
+	defer{ std::system("PAUSE"); };
+
+#if defined(TEST_PARSING_SPEED)
+	test_parsing_speed();
+#endif
 
     if (utilities::read_all_file("./compiler-f/main.f", input_file_content) == false) {
         return 1;
@@ -39,6 +92,5 @@ int main(int ac, char** av)
 
 //    generate_hello_world();
 
-//    std::system("PAUSE");
 	return 1;
 }
