0fe2dba184688f02ba664438079a1474c90fb952
Flamaros <flamaros.xavier@gmail.com>
2024-04-25 21:38:07 +0200

    Create new project nasm_db_to_code
    There is nothing inside for the moment

---
+++ compiler-cpp/sources/f-asm_nasm_db_to_code/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <tracy/Tracy.hpp>
+
+int main(int ac, char** av)
+{
+#if defined(TRACY_ENABLE)
+	tracy::SetThreadName("Main thread");
+#endif
+	FrameMark;
+
+
+
+	FrameMark;
+	return 0;
+}
